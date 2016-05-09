c24e277d198b7dff7cbf51eff472c3bfb099f71c
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-09 01:02:04 -0700

    Create a separate navbar controller and move vm.isLoggedIn and vm.logout into it

--- src/app/layout/navbar.directive.js
+++ src/app/layout/navbar.directive.js
@@ -8,7 +8,23 @@
   function trNavbar() {
     return {
       templateUrl: 'app/layout/navbar.html',
-      restrict: 'E'
+      restrict: 'E',
+      controller: NavBarController,
+      controllerAs: 'vm'
+    }
+  }
+  
+  NavBarController.$inject = ['$location', 'authService'];
+  
+  function NavBarController($location, authService) {
+    var vm = this;
+    
+    vm.isLoggedIn = authService.isLoggedIn;
+    vm.logout = logout;
+    
+    function logout() {
+      authService.logout();
+      $location.path('/');
     }
   }
   
