7383a7a574ac4c8ec6247ae9e2bd625ea415b5ac
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-09 01:03:11 -0700

    Clean up unneeded code

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -17,8 +17,6 @@
     
     vm.register = register;
     vm.login = login;
-    vm.logout = logout;
-    vm.isLoggedIn = authService.isLoggedIn;
 
     function register(user) {
       return authService.register(user)
@@ -41,11 +39,6 @@
         });
     }
     
-    function logout() {
-      console.log('logging out');
-      authService.logout();
-      $location.path('/');
-    }
   }
   
 }());
