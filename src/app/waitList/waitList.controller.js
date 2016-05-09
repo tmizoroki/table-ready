338cb2b1ac9f735ca32cc17e9399085bf76e7658
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-08 21:15:22 -0700

    Refactor WaitListController to use textMessageService

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -5,9 +5,9 @@
     .module('app.waitList')
     .controller('WaitListController', WaitListController);
   
-  WaitListController.$inject = ['firebaseDataService', 'partyService'];
+  WaitListController.$inject = ['textMessageService', 'partyService'];
   
-  function WaitListController(firebaseDataService, partyService) {
+  function WaitListController(textMessageService, partyService) {
     var vm = this;
     
     vm.newParty = new partyService.Party();
@@ -27,15 +27,7 @@
     }
     
     function sendTextMessage(party) {
-      var newTextMessage = {
-        name: party.name,
-        phoneNumber: party.phone,
-        size: party.size
-      }
-      firebaseDataService.textMessages.push(newTextMessage);
-      
-      party.notified = true;
-      vm.parties.$save(party);
+      textMessageService.sendTextMessage(party, vm.parties);
     }
 
     function toggleDone(party) {
