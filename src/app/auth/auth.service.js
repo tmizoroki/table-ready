9b315724112c7be1ad5ef3988253d8a53f4ad172
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-08 21:24:55 -0700

    Setup authService off of the app.auth module and add it to index.html

---
+++ src/app/auth/auth.service.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  
+  angular
+    .module('app.auth')
+    .factory('authService', authService);
+  
+  authService.$inject = [];
+  
+  function authService() {
+    var service = {};
+    
+    return service;
+    
+    ////////////
+    
+    
+  }
+  
+})();
