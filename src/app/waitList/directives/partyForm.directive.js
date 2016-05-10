21c6f18ddb74b1fd84c1bec41cf801c667dd5f38
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-10 04:41:48 -0700

    Move applicable controller functions from WaitListController into PartyFormController

--- src/app/waitList/directives/partyForm.directive.js
+++ src/app/waitList/directives/partyForm.directive.js
@@ -1,15 +1,35 @@
 (function() {
   'use strict';
-  
+
   angular
     .module('app.waitList')
-    .directive('trPartyForm', trPartyForm);
-  
-  function trPartyForm() {
+    .directive('tmPartyForm', tmPartyForm);
+
+  function tmPartyForm() {
     return {
       templateUrl: 'app/waitList/directives/partyForm.html',
-      restrict: 'E'
+      restrict: 'E',
+      controller: PartyFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        parties: '='
+      }
     };
   }
-  
+
+  PartyFormController.$inject = ['partyService'];
+
+  function PartyFormController(partyService) {
+    var vm = this;
+
+    vm.newParty = new partyService.Party();
+    vm.addParty = addParty;
+
+    function addParty() {
+      vm.parties.$add(vm.newParty);
+      vm.newParty = new partyService.Party();
+    }
+  }
+
 })();
